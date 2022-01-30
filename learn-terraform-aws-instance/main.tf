25eb62c4ac18d2e5f9d670749fd7454aefe34005
Marc Prewitt <marc@chartbeat.com>
2022-01-30 14:57:37 -0500

    adding vpc/sg for ssh access (WIP)

--- learn-terraform-aws-instance/main.tf
+++ learn-terraform-aws-instance/main.tf
@@ -16,12 +16,59 @@ provider "aws" {
 variable "instance_name" {
   description = "Value of the Name tag for the EC2 instance"
   type        = string
-  default     = "ExampleAppServerInstance"
+  default     = "MarcTerraformTesting"
+}
+
+resource "aws_vpc" "marc-ssh-test" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+  tags {
+    name = "marc-tf-test"
+  }
+}
+
+resource "aws_subnet" "marc-tf-test" {
+  cidr_block = "${cidrsubnet(awc_vpc.marc-ssh-test.cidr_block, 3, 1)}"
+  vpc_id     = aws_vpc.marc-ssh-test.id
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_security_group" "marc-test-ssh" {
+  name = "marc-ssh-allow-sg"
+  vpc_id = aws_vpc.marc-ssh-test.id
+  ingress {
+    from_port = 22
+    protocol  = "tcp"
+    to_port   = 22
+    cidr_blocks = ["98.113.29.206/32"]
+  }
+  egress {
+    from_port = 0
+    protocol  = "-1"
+    to_port   = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "app_server" {
   ami           = "ami-0ed9277fb7eb570c9"
   instance_type = "t2.micro"
+  key_name = "marc-test"
+  subnet_id = "${aws_security_group.marc-test-ssh.id}"
+  provisioner "local-exec" {
+    command = "echo ip=${self.private_ip}"
+  }
+  connection {
+    type = "ssh"
+    host = self.public_ip
+    user = "ec2-user"
+    private_key = file("marc-test.pem")
+  }
+  provisioner "file" {
+    content = "foobar"
+    destination = "/tmp/foobar.txt"
+  }
   tags = {
     Name = var.instance_name
   }
